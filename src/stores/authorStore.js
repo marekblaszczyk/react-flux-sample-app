aae30c63e064b15e9164277bef691402abe30ad8
Marek Błaszczyk <marek.blaszczyk@gmail.com>
2016-02-27 16:19:47 +0100

    This is the basic template for any Flux store

--- src/stores/authorStore.js
+++ src/stores/authorStore.js
@@ -3,4 +3,27 @@
 var Dispatcher = require('../dispatcher/appDispatcher');
 var ActionType = require('../constants/actionTypes');
 var EventEmitter = require('events').EventEmitter;
+var assign = require('object-assign');
+var CHANGE_EVENT = 'change';
 
+var AuthorStore = assign({}, EventEmitter.prototype, {
+
+    addChangeListener: function(callback) {
+        this.on(CHANGE_EVENT, callback);
+    },
+    removeChangeListener: function(callback) {
+        this.removeListener(CHANGE_EVENT, callback);
+    },
+    emitChange: function() {
+        this.emit(CHANGE_EVENT);
+    }
+});
+
+Dispatcher.register(function(action) {
+
+    switch(action.actionType) {
+
+    }
+});
+
+module.exports = AuthorStore;
